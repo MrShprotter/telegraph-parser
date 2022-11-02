3bb317ac8fbeea2e9bd235ad378b25f5ef909404
Gleb <lazey07@proton.me>
2022-11-02 13:57:07 +0300

    Upd

--- main.sh
+++ main.sh
@@ -1,13 +1,48 @@
 #!/bin/bash
-python main.py $(gum input --placeholder "Link") > links.txt
+lcount=0
+larr=()
+vall=0
+for link in $(gum write --placeholder "Links")
+do
+    python main.py $link >> links$lcount.txt
+    larr+=(links$lcount.txt)
+    lcount=$(( $lcount+1 ))
+    echo ${larr[@]}
+    echo $lcount
+done
+
+function find_in_arr {
+    for hm in $patts
+    do
+        if [ $1 == $hm ]; then
+            vall=1
+        fi
+    done
+}
+
+patts=$(gum write --placeholder "Igore pattern")
 
 count=0
+loc_count=0
 arr=()
-
-for var in $(cat links.txt)
+for ff in ${larr[@]}
 do
-    wget $var -O $count.png
-    arr[$count]=$count.png
-    count=$(( count+1 ))
+    echo ${larr[@]}
+    loc_count=0
+    for var in $(cat $ff)
+    do
+        loc_count=$(( loc_count+1 ))
+        find_in_arr $loc_count
+        if [ $vall == 0 ]; then
+            wget $var -O $count.png
+            arr[$count]=$count.png
+            count=$(( count+1 ))
+        fi
+        vall=0
+        echo $ff
+    done
 done
+
 tar -cf $(gum input --placeholder "CBT name").cbt ${arr[@]}
+rm *.png
+rm *.txt
